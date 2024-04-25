f89f5f0f45fb82773bc1b5badd0390cdf0f14629
liannajane <157860289+liannajane@users.noreply.github.com>
2024-04-24 23:05:18 -0400

    page changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,32 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+// test: console.log("hello!");
+//from function tutorial
+/*const nextPage = document.querySelector('.greece-museum');
+console.log(nextPage);
+nextPage.classList.toggle("greece-museum");
+console.log(nextPage)
+function toggleParagraph() {
+	nextPage.classList.toggle("greece-museum");
+	console.log(nextPage);
+
+}*/
+
+/*from chatgpt*/
+let currentQuestionIndex = 0;
+const pages = document.querySelectorAll('.page');
+
+function nextPage() {
+	for (const page of pages) {
+		page.classList.add('hidden');
+	}
+	pages[currentQuestionIndex].classList.remove('hidden');
+	currentQuestionIndex++;
+
+}
+nextPage();
+nextPage();
+nextPage();
+nextPage();
+//add eventListener
